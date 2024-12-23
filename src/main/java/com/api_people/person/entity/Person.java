af1784e8569642d2250a8f8c64ca8af56fee7140
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-12-23 20:10:38 -0300

    created entity Person

---
+++ src/main/java/com/api_people/person/entity/Person.java
@@ -0,0 +1,73 @@
+package com.api_people.person.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String name;
+    private String birthDate;
+    private String addres;
+
+    public Person(Long id, String name, String birthDate, String addres) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.addres = addres;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+}
